cb18a73bbe37ad87cfdf496fb4900af6abef244f
Jorge Lerma <jorge.lerma@globant.com>
2019-06-06 22:55:25 -0500

    Refactors classes to kotlin

---
+++ app/src/main/java/com/globant/counter/android/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.globant.counter.android
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.globant.counter.android.mvp.model.CountModel
+import com.globant.counter.android.mvp.presenter.CountPresenter
+import com.globant.counter.android.mvp.view.CountView
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity: AppCompatActivity() {
+
+    lateinit var presenter: CountPresenter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val countViews = CountView(this)
+        presenter = CountPresenter(CountModel(), countViews)
+
+        count_button.setOnClickListener {
+            countViews.countButtonPressed()
+        }
+
+        reset_button.setOnClickListener {
+            countViews.resetButtonPressed()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        presenter.register()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        presenter.unregister()
+    }
+
+}
